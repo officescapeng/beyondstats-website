c2a95d0129d3e50abb53e772b437dbf41327439a
officescapeng <officescapeng@gmail.com>
2026-07-04 21:06:34 +0100

    homepage

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MaintenancePage from './MaintenancePage.jsx'
+
+const isMaintenanceMode = import.meta.env.VITE_MAINTENANCE_MODE === 'true'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {isMaintenanceMode ? <MaintenancePage /> : <App />}
   </StrictMode>,
 )
