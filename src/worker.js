ea4bc0c24c629df112304dc29c33bff2b132ddd4
officescapeng <officescapeng@gmail.com>
2026-07-08 09:02:42 +0100

    404 update

---
+++ src/worker.js
@@ -0,0 +1,10 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const response = await env.ASSETS.fetch(request);
+    if (response.status === 404) {
+      return await env.ASSETS.fetch(new Request(new URL("/index.html", request.url), request));
+    }
+    return response;
+  }
+};
