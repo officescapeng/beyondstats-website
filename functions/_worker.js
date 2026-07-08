4473ce3779297f8d053776705f25552cf4baaceb
officescapeng <officescapeng@gmail.com>
2026-07-08 10:36:39 +0100

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -1,8 +1,3 @@
 export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  const response = await context.env.ASSETS.fetch(context.request);
-  if (response.status === 404) {
-    return context.env.ASSETS.fetch(new URL("/index.html", url));
-  }
-  return response;
+  return new Response("Function working", { status: 200 });
 }
