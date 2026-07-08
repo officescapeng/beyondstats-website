317c701b214dff0f7f4a585214b1aecb4d58d0a7
officescapeng <officescapeng@gmail.com>
2026-07-08 10:28:13 +0100

    rederiects

--- functions/_worker.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  const { request, env } = context;
-  const response = await env.ASSETS.fetch(request);
-  if (response.status === 404) {
-    return env.ASSETS.fetch(new URL("/index.html", request.url));
-  }
-  return response;
-}
